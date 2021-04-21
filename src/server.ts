a41964502304e94e40a052ad324f548a6e053a16
Rafael Pelegrini <arrobarafael@hotmail.com>
2021-04-20 22:32:22 -0300

    Criada estrutura de banco de dados, migrations, entidades, repositórios e controllers

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,11 @@
 import express from 'express';
 
+import './database';
+import { routes } from './routes';
+
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.send('get');
-});
+app.use(express.json());
+app.use(routes);
 
-app.post('/', (request, response) => {
-  return response.send('post');
-});
-
-app.listen(3333, () => console.log('Server running'));
+app.listen(3333, () => console.log('Server is running'));
